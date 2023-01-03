0cfae70c55354cf803920d14432ab5bf0da10419
Jmakwana01 <jaymakwanaworks@gmail.com>
2023-01-03 20:51:33 +0530

    first-commit

---
+++ contracts/NFTAirdrop.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract NFTAirdrop is ERC721Enumerable,Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NFT Airdrop Demo","NAD"){
+        console.log("Contract has being deployed!");
+    }
+
+    function airdropNFTs(address[] calldata wAddress)public onlyOwner{
+        for(uint i=0;i<wAddress.length;i++){
+            _mintSingleNFT(wAddress[i]);
+        }
+    }
+    function _mintSingleNFT(address wAddress)private{
+        uint newTokenID=_tokenIds.current();
+        _safeMint(wAddress, newTokenID);
+        _tokenIds.increment();
+    }
+}
